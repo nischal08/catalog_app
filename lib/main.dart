9ffa9aa38fc07e18f20b5edcd78e9352abc806e6
nischal08 <nischal.karanjit@gmail.com>
2021-06-01 17:48:15 +0545

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text('Welcome to the catalog app'),
+          ),
+        ),
+      ),
+    );
+  }
+}
